2526f47926268cc1e7ecd7085dbc160fe2a249e7
VladimirTulenev2325 <beatstours2325@yandex.ru>
2023-06-07 13:19:26 +0300

    22

---
+++ app/src/main/java/android/bignerdranch/chat/MainActivity.java
@@ -0,0 +1,42 @@
+package android.bignerdranch.chat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView chatTextView;
+    private EditText messageEditText;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chatTextView = findViewById(R.id.chatTextView);
+        messageEditText = findViewById(R.id.messageEditText);
+        sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageEditText.getText().toString().trim();
+                if (!message.isEmpty()) {
+                    appendMessage(message);
+                    messageEditText.setText("");
+                }
+            }
+        });
+    }
+
+    private void appendMessage(String message) {
+        String currentText = chatTextView.getText().toString();
+        String newText = currentText + "\n" + message;
+        chatTextView.setText(newText);
+    }
+}
